704bb779ac58fab34fdb4984c3f4612b01f7bd55
joykangangi <64706463+joykangangi@users.noreply.github.com>
2023-09-19 16:41:25 +0300

    set up multi-module

---
+++ feature/calendar/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.feature.calendar"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 27
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        maybeCreate("release").apply {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    /*Standard Dependencies*/
+    implementation(Libs.core_ktx)
+    implementation(Libs.core_appcompat)
+    implementation(Libs.compose_material3)
+    implementation(Libs.compose_material_icons)
+    testImplementation(Libs.junit)
+    testImplementation(Libs.junit_engine)
+    androidTestImplementation(Libs.junit_android)
+    androidTestImplementation(Libs.espresso)
+}
