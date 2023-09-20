c3408299c082d9ca73f016f7745973a45cc90d85
sojious <soj.ykn@gmail.com>
2023-09-20 18:45:07 +0100

    implemented navigation logic

--- app/src/main/java/com/evolver/eventsapp/ui/theme/Type.kt
+++ app/src/main/java/com/evolver/eventsapp/ui/theme/Type.kt
@@ -2,11 +2,15 @@ package com.evolver.eventsapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.evolver.eventsapp.R
 
 // Set of Material typography styles to start with
+
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +35,7 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+
 )
+
+val latoFont = Font(R.font.lato_regular,FontWeight.Normal)
