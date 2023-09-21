0450e0a40d991d0aa0120fb8caf723a027e99a1d
Edidiong Udoh <gisthive@gmail.com>
2023-09-21 13:15:30 +0100

    local

--- app/src/main/java/com/evolver/eventsapp/ui/theme/comment/comment.kt
+++ app/src/main/java/com/evolver/eventsapp/ui/theme/comment/comment.kt
@@ -29,7 +29,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -92,7 +91,7 @@ fun SettingsCard(){
                     },
                 ) {
                     Image(
-                        painter = painterResource(id = R.drawable.menu),
+                        painter = painterResource(id = R.drawable.menur),
                         contentDescription = "image",
                         contentScale = ContentScale.Fit,
                         modifier = Modifier
