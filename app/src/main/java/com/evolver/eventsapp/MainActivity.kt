0de15d8457bd0cf581bef07994101b710b7e13bb
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2023-09-22 09:07:54 +0100

    feat: merged dev into people

--- app/src/main/java/com/evolver/eventsapp/MainActivity.kt
+++ app/src/main/java/com/evolver/eventsapp/MainActivity.kt
@@ -3,10 +3,14 @@ package com.evolver.eventsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.evolver.eventsapp.ui.onboarding.SplashScreen
 import com.evolver.eventsapp.people.PeopleScreen
